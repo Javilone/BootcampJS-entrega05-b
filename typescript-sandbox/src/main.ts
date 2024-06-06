22ce1d38078a583fe7b986ab823bf2db4655f2ad
Javilone <javilone.blummer@gmail.com>
2024-06-06 17:06:09 +0200

    Terminadas todas las funciones esenciales

--- typescript-sandbox/src/main.ts
+++ typescript-sandbox/src/main.ts
@@ -50,17 +50,17 @@ const getACard = (card : number) => {
   return cardUrl;
 };
 
-/* getRandomCard obtiene un número aleatorio */
+/* getRandomCard genera un número aleatorio */
 const getRandomNumber = () => {
   return Math.ceil(Math.random() * 10);
 
 }
- /* getRandomCard obtiene la carta */
+ /* getRandomCard obtiene la carta aleatoria */
 const getRandomCard = (randomNumber: number) => { 
   return randomNumber > 7 ? randomNumber + 2 : randomNumber;
 }
 
-/* createCard crea la carta que será pintada en el contenedor padre */
+/* createCard crea la carta y su clase que será pintada en el contenedor padre */
 const createCard = (cardUrl : string) => {
   const playedCard = document.createElement("img");
   playedCard.src = cardUrl;
@@ -82,13 +82,118 @@ const getCardValue = (randomCard: number) => {
   return randomCard > 7 ? 0.5 : randomCard;
 }
 
-/* Devuelve el valor de la carta */
+/* Obtiene el valor de la carta */
 const getPoints = (randomCard : number) => {
   const value = getCardValue(randomCard);
-  return totalPoints = totalPoints + value;
+  return totalPoints + value;
 }
 
-/* playGame inicia el juego */
+/* Establece el valor de la carta en la variable totalPoints */
+const setPoints = (getPoints : number) =>  {
+  totalPoints = getPoints;
+}
+
+/* Actualiza el heading element del html con la puntuación */
+const updateScore = (totalPoints : string)  => {
+  if (score !== undefined && score !== null && score instanceof HTMLHeadingElement) {
+      score.innerHTML = totalPoints.toString();
+  }
+}
+
+/* Determina el estado de la partida según la puntuación */
+const gameStatus = () => {
+  let statusMessage = "";
+
+  if(totalPoints === 7.5) {
+    statusMessage = `¡Lo has clavado! ¡Enhorabuena! Has conseguido ${totalPoints} puntos!`;
+    giveCardButtonOff();
+    giveUpButtonOff();
+  } else if (totalPoints > 7.5) {
+    statusMessage = `¡Te pasaste! Has obtenido ${totalPoints} puntos.`
+    giveCardButtonOff();
+    giveUpButtonOff();
+  } else {
+      statusMessage = totalPoints.toString();
+  }
+  return statusMessage;
+}
+
+/* Deshabilita el botón de Pedir Carta */
+const giveCardButtonOff = ()  => {
+  if (giveCardBtn !== null && giveCardBtn !== undefined
+    && giveCardBtn instanceof HTMLButtonElement) {
+      giveCardBtn.className = "play-btn-off";
+      giveCardBtn.disabled = true;
+  }  
+}
+
+/* Deshabilita el botón de Plantarse */
+const giveUpButtonOff = () => {
+  if (giveUpBtn !== null && giveUpBtn !== undefined
+    && giveUpBtn instanceof HTMLButtonElement) {
+    giveUpBtn.className = "play-btn-off";
+    giveUpBtn.disabled = true;
+    }
+}
+
+/* Evalúa el mensaje de estado de la partida al momento de plantarse */
+const getGiveUpStatus = () => {
+  giveUpButtonOff();
+  if(totalPoints <= 4) {
+      return `Has sido muy conservador. <br><p>Pulsa "Una más" para saber qué habrías sacado.</p><button id="one-more" class="play-btn">Una más</button>`;
+  } 
+  if(totalPoints >= 4.5 && totalPoints <= 5.5) {
+      return `Te ha entrado el canguelo ¿eh? <br><p>Pulsa "Una más" para saber qué habrías sacado.</p><button id="one-more" class="play-btn">Una más</button>`;
+  } 
+  if(totalPoints >= 6 && totalPoints <= 7) {
+      return `Casi casi...<br><p> Pulsa "Una más" para saber qué habrías sacado.</p><button id="one-more" class="play-btn">Una más</button>`;
+  } 
+  return totalPoints.toString();
+}
+
+/* Establece el mensaje de estado tras plantarse */
+const setGiveUpStatus = (gameStatus : string) => {
+  const giveUpPanel = document.getElementById("give-up-panel");
+  if (giveUpPanel instanceof HTMLDivElement
+    && giveUpPanel !== null && giveUpPanel !== undefined) {
+    giveUpPanel.className = "give-up-panel-on";
+    giveUpPanel.innerHTML = gameStatus;
+    oneMoreTimeButton();
+  }
+}
+
+/* Habilita el botón de "Una más" en el panel emergente tras plantarse */
+const oneMoreTimeButton = () => {
+  const oneMore = document.getElementById("one-more");
+  if (oneMore instanceof HTMLButtonElement
+    && oneMore !== null && oneMore !== undefined)
+  {
+    oneMore.addEventListener("click", getOneMoreTime);
+  }
+}
+
+/* Arranca el proceso de jugar una carta más */
+const getOneMoreTime = () => {
+  const randomNumber : number = getRandomNumber();
+  const randomCard : number = getRandomCard(randomNumber);
+  const cardUrl: string = getACard(randomCard);
+  const value = getCardValue(randomCard)
+  
+  const playedCard = createCard(cardUrl);
+  drawCard(playedCard);
+
+  let gameStatus = `Pues que habrías sacado un ${value}.<br>Y en total tendrías ${totalPoints + value} puntos`;
+  setGiveUpStatus(gameStatus);
+}
+
+/* Inicia el proceso de plantar la partida */
+const giveUp = ()  => {
+  const gameStatus = getGiveUpStatus()
+  setGiveUpStatus(gameStatus);
+  giveCardButtonOff();
+}
+
+/* playGame arranca el juego */
 const playGame = () => {
   const randomNumber : number = getRandomNumber();
   const randomCard : number = getRandomCard(randomNumber);
@@ -98,11 +203,13 @@ const playGame = () => {
   drawCard(playedCard);
 
   const getScore = getPoints(randomCard);
+  setPoints(getScore);
 
-  console.log(getScore)
+  const statusMessage = gameStatus();
+  updateScore(statusMessage);
 }
 
-/* Simplemente recarga la página */
+/* Recarga la página */
 const restartGame = () => {
   location.reload();
 }
@@ -110,6 +217,7 @@ const restartGame = () => {
 // ELEMENTOS DEL DOM
 const score = document.getElementById("score");
 const giveCardBtn = document.getElementById("play-btn");
+const giveUpBtn = document.getElementById("give-up-btn");
 const restartGameBtn = document.getElementById("restart-btn");
 
 // ASIGNACIÓN DE FUNCIONES A LOS ELEMENTOS
@@ -121,6 +229,11 @@ if(giveCardBtn instanceof HTMLButtonElement
   && giveCardBtn !== null && giveCardBtn !== undefined) {
   giveCardBtn.addEventListener("click", playGame);
 }
+
+if (giveUpBtn instanceof HTMLButtonElement
+  && giveUpBtn !== null && giveUpBtn !== undefined) {
+  giveUpBtn.addEventListener("click", giveUp);
+  }
   
 if(restartGameBtn instanceof HTMLButtonElement
   && restartGameBtn !== null && restartGameBtn !== undefined) {
